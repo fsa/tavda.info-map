664e62012952d443a9319bf371fe1c93034841dc
FSA <fsa@console.tavda.info>
2026-07-07 16:54:41 +0500

    Initial commit: Astro site with Leaflet map for Tavda

---
+++ src/components/MapSearch.tsx
@@ -0,0 +1,12 @@
+export default function MapSearch() {
+  return (
+    <div className="search-wrapper">
+      <label className="search-label">Поиск</label>
+      <input
+        type="text"
+        placeholder="Поиск"
+        className="search-input"
+      />
+    </div>
+  );
+}
