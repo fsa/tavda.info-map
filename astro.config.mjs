462211dcb5c2049f5646fbe7b9c680899439f80a
FSA <fsa@console.tavda.info>
2026-07-07 17:44:03 +0500

    Add GitHub Pages workflow and CNAME for map.tavda.info

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://map.tavda.info",
   integrations: [react()],
   vite: {
     ssr: {
